e000148efd947a5ab06523f919a9db97cea405e9
Abhinav Jaiswal <114914635+AbhinavJais@users.noreply.github.com>
2023-11-30 14:42:25 +0530

    updated JS and html

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const timeEL = document.getElementById("time");
 const dateEL = document.getElementById("date");
 const currentweatheritemsEL = document.getElementById("current-weather-items");
-const timezoneEL = document.getElementById("time-zone");
+// const timezoneEL = document.getElementById("time-zone");
 const countryEL = document.getElementById("country");
 const forecastEl = document.getElementById("forecast");
 const currenttempEl = document.getElementById("current-temp");
-
+const searchBox = document.querySelector(".search-bar input");
+const searchBtn = document.querySelector(".search-bar button");
+const bgImage = document.querySelector("body");
+const weatherIcon = document.querySelector("w-icon");
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturday"];
 const months = [
   "Jan",
@@ -48,33 +51,71 @@ const API_URL =
   "https://api.openweathermap.org/data/2.5/forecast?q=noida&units=metric&exclude=hourly,minutely";
 
 function getWeatherData() {
-  //  navigator.geolocation.getCurrentPosition((success) => {
-  //      let {latitude, longitude} = success.coords;
-  //      console.log(success);
+  navigator.geolocation.getCurrentPosition((success) => {
+    let { latitude, longitude } = success.coords;
+    console.log(success);
+  });
 }
 
-fetch(API_URL + `&appid=${API_KEY}`)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    let city = data.city.name;
-    let main = data.list[0].main;
-    let temp = data.list[0].main.temp;
-    let weather = data.list[0].weather[0].description;
-    console.log(weather);
-    console.log(temp);
-    console.log(main);
-    console.log(city);
-    document.querySelector(".ttemp").innerHTML = data.list[0].main.temp;
-    document.querySelector(".Humidity").innerHTML = data.list[0].main.humidity;
-    document.querySelector(".Pressure").innerHTML = data.list[0].main.pressure;
-    document.querySelector(".Wind").innerHTML = data.list[0].wind.speed;
-    // document.querySelector(".sun-rise").innerHTML = data.city.sunrise;
-    // document.querySelector(".sun-set").innerHTML = data.city.sunset;
-    //  showWeatherData()
-  });
+function checkWeather() {
+  fetch(API_URL + `&appid=${API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let city = data.city.name;
+      let main = data.list[0].main;
+      let temp = data.list[0].main.temp;
+      let weather = data.list[0].weather[0].main;
+      console.log(weather);
+      console.log(temp);
+      console.log(main);
+      console.log(city);
+      document.querySelector(".ttemp").innerHTML =
+        Math.round(data.list[0].main.temp) + "°C";
+      document.querySelector(".Humidity").innerHTML =
+        data.list[0].main.humidity + "%";
+      document.querySelector(".Pressure").innerHTML =
+        data.list[0].main.pressure + " hPa";
+      document.querySelector(".Wind").innerHTML =
+        data.list[0].wind.speed + " km/h";
+      document.querySelector(".city").innerHTML = data.city.name;
+      // document.querySelector(".sun-rise").innerHTML = data.city.sunrise;
+      // document.querySelector(".sun-set").innerHTML = data.city.sunset;
 
-getWeatherData();
+
+
+      //for the changing background according to the weather.
+      switch (weather) {     
+        case "Snow":
+          bgImage.style.backgroundImage = "url(snow.gif)";
+          break;
+        case "Clouds":
+          bgImage.style.backgroundImage = "url(clouds.gif)";
+          break;
+        case "Clear":
+          bgImage.style.backgroundImage = "url(clear.gif)";
+          break;
+        case "Fog":
+          bgImage.style.backgroundImage = "url(fog.gif)";
+          break;
+        case "Rain":
+          bgImage.style.backgroundImage = "url(rain.gif)";
+          break;
+        case "Thunderstrom":
+          bgImage.style.backgroundImage = "url(thunderstrom.gif)";
+          break;
+        default:
+          bgImage.style.backgroundImage = "url(clear.gif)";
+          break;
+      }
+    });
+}
+checkWeather();
+
+//for searching of city
+// searchBtn.addEventListener("click", ()=>{
+//   checkWeather(searchBox.value);
+// })
 
 function showWeatherData(data) {
   let { humidity, pressure, sunrise, sunset, wind_speed } = data.city;
@@ -83,18 +124,10 @@ function showWeatherData(data) {
   currentweatheritemsEL.innerHTML = `div class="weather-items">
   div class="weather-item">
         <div>Sunrise</div>
-        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+        <div>${window.moment(sunrise * 1000).format("HH:mm a")}</div>
     </div>
     <div class="weather-item">
         <div>Sunset</div>
-        <div>${window.moment(sunset*1000).format('HH:mm a')}</div>
+        <div>${window.moment(sunset * 1000).format("HH:mm a")}</div>
     </div>`;
 }
-
-// switch (weather) {
-//   case "Snow":
-//     break;
-
-//   default:
-//     break;
-// }
